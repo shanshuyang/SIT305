720fbe7de677f4f76bfeb34cb9e967fe70fc337c
shanshuyang <1250619944@qq.com>
2021-05-17 13:53:50 +0800

    Task8.1C project and video

---
+++ Task8.1C/YouTubePlayerApp/app/src/main/java/com/example/youtubeplayerapp/VideoActivity.java
@@ -0,0 +1,41 @@
+package com.example.youtubeplayerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class VideoActivity extends YouTubeBaseActivity {
+    Intent getURL;
+    String url;
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        getURL = getIntent();
+        url = getURL.getStringExtra("URL");
+        youTubePlayerView = findViewById(R.id.youTubePlayerView);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo(url);
+                Toast.makeText(VideoActivity.this,"Initialization successful", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(VideoActivity.this,"Initialization failed", Toast.LENGTH_LONG).show();
+            }
+        };
+        youTubePlayerView.initialize(YouTubeConfig.getApiKey(), onInitializedListener);
+    }
+}
---
+++ Task8.1C/YouTubePlayerApp/app/src/main/java/com/example/youtubeplayerapp/YouTubeConfig.java
@@ -0,0 +1,12 @@
+package com.example.youtubeplayerapp;
+
+public class YouTubeConfig {
+    public YouTubeConfig(){
+    }
+
+    private static final String API_KEY = "AIzaSyB9GwTWNXnPndsHqEe94whdHSmY8bGgeWE";
+
+    public static String getApiKey(){
+        return API_KEY;
+    }
+}
